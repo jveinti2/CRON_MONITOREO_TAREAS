e732a3e984001760f33c794e5f0ca6bf63417007
Jhonatan S. Becerra <72992939+jveinti2@users.noreply.github.com>
2024-04-24 15:27:19 -0500

    ok

--- utils/constantes.js
+++ utils/constantes.js
@@ -15,8 +15,8 @@ var constantes = {
     UNOEE: 'UnoEE_Dllo',
     WWWBD:'MDC_WWW',
     AWS: {
-        accessKeyId: 'AKIAJ53CG32YCV4T4ZWA',
-        secretAccessKey: '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'
+        accessKeyId: ,
+        secretAccessKey: 
     }
 };
 
